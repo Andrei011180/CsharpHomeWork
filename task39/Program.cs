4f8dc640ce613c6f6c4f10916981d4d4f7c29254
Andrei <reysavin@yandex.ru>
2023-10-26 01:38:17 +1000

    Задачи семинара 5

---
+++ task39/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+Напишите программу , которая перевернет одномерный массив 
+*/
+// Console.Clear();
+
+// int[ ] GetRnd(int size)
+// {
+//     int[ ] array = new int[size];
+//     for(int i = 0; i < size; i++)
+//     {
+//         array[i] = new Random().Next(0, 10);
+//     }
+//     return array;
+// }
+// void PrintArray(int[ ] arr)
+// {
+//     for (int i= 0; i < arr.Length; i++)
+//     {
+//         System.Console.WriteLine(arr[i]+ " ");
+//     }
+// }
+// void Rev( int [ ] arr)
+// {
+//     for(int i = 0; i< arr.Length / 2; i++)
+//     {
+//         int temp = arr [i];
+//         arr[i] = arr[arr.Length - 1 - i];
+//         arr[arr.Length - 1 - i] = temp;
+//     }
+// }
+
+// int ArrayRnd = GetRnd(10);
+// PrintArray(ArrayRnd);
+// System.Console.WriteLine();
+// Rev(ArrayRnd);
+// System.Console.WriteLine();
+// PrintArray(ArrayRnd);
