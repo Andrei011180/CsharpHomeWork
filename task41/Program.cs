443ae3f12be0fb04d2d286b2d9ffb804e22d325b
Andrei <reysavin@yandex.ru>
2023-10-27 01:26:39 +1000

    Задачи семинара 6

---
+++ task41/Program.cs
@@ -0,0 +1,41 @@
+﻿/*
+Пользователь вводит с клавиатуры М чисел . Посчитайте , сколько чисел больше 0 ввел пользователь
+*/
+
+
+// int[ ] array = UserArray();
+// PrintArray(array);
+// System.Console.WriteLine(" -> " + NewArray(array));
+
+// int[ ] UserArray()
+// {
+//     System.Console.WriteLine(" Введите длинну массива : ");
+//     int length = Convert.ToInt32(Console.ReadLine());
+//     int [ ] array = new int[ length ];
+//     for( int i = 0; i < length; i++)
+//     {
+//         System.Console.Write($" Элемент {i} из { length - 1 } : ");
+//     }
+//     return array;
+// }
+
+// void PrintArray(int[ ] array)
+// {
+//     System.Console.Write( " [ ");
+//     for(int i = 0; i< array.Length; i++)
+//     {
+//         System.Console.Write(array[i]);
+//         if(i<array.Length - 1) System.Console.Write(", ");
+//     }
+//     System.Console.Write(" ] ");
+// }
+
+// int NewArray(int[ ] array)
+// {
+//     int result = 0;
+//     for(int i = 0; i< array.Length; i++)
+//     {
+//         if(array[i] > 0) result++;
+//     }
+//     return result;
+// }
---
+++ task43/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Напишите программу , которая найдет точку пересечения двух прямых ,
+заданных уравнением y=k1*x+b1, y=k2*x+b2 . Значения  b1, k1, b2, k2 задаются пользователем
+*/
+
+// System.Console.WriteLine(" Введите значение b1 ");
+// double b1 = Convert.ToDouble(Console.ReadLine());
+
+// System.Console.WriteLine(" Введите значение k1 ");
+// double k1 = Convert.ToDouble(Console.ReadLine());
+
+// System.Console.WriteLine(" Введите значение b2 ");
+// double b2 = Convert.ToDouble(Console.ReadLine());
+
+// System.Console.WriteLine(" Введите значение k2 ");
+// double k2 = Convert.ToDouble(Console.ReadLine());
+
+// /*
+// Решение для полуения координат
+// y=k1*x+b1, y=k2*x+b2
+// k1*x+b1=k2*x+b2
+// (k1-k2)*x=b2-b1
+// x=(b2-b1) / (k1-k2)
+// y=k1*x+b1
+// */
+
+// double x = (b2-b1) / (k1-k2);
+// double y = k1*x+b2;
+// System.Console.Write($" Точка пересечения прямых находится в координатах x = {x} ; y = {y} ");
