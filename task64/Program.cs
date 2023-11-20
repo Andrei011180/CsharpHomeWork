c6a0b90ad2731c35c50d1af3950a2aed8558f845
Andrei <reysavin@yandex.ru>
2023-11-20 23:34:56 +1000

    Промежуточная аттестация

---
+++ task64/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Задача 64: Задайте значение N. Напишите программу, которая выведет все
+натуральные числа в промежутке от N до 1.
+Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+
+// void DigitDiapazon(int number, int start = 1)
+// {
+//     if (number < start)
+//     {
+//         return;
+//     }
+//     else
+//     {
+//         System.Console.Write(number + " ");
+//         DigitDiapazon(number - 1);
+//         number = number - 1;
+//     }
+// }
+
+// DigitDiapazon(6);
+
+
---
+++ task66/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных
+ элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+// int userNumberM = 4 ;
+// int userNumberN = 8 ;
+// int sum = SumDigit(userNumberM, userNumberN);
+// System.Console.WriteLine(sum);
+
+// int SumDigit(int numberM, int numberN)
+// {
+//     if(numberM == numberN) return numberM;
+//     else return numberM + SumDigit(numberM + 1, numberN);
+// }
